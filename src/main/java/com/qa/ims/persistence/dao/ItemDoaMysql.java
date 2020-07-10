073081820e3a981e6fa621ba2f732aadf52276c9
James Monks <James.Monks@academytrainee.com>
2020-07-10 12:45:32 +0100

    made Item, ItemController, ItemServices and ItemDaoMysql files

---
+++ src/main/java/com/qa/ims/persistence/dao/ItemDoaMysql.java
@@ -0,0 +1,147 @@
+package com.qa.ims.persistence.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import com.qa.ims.persistence.domain.Item;
+import com.qa.ims.utils.Utils;
+
+public class ItemDoaMysql implements Dao<Item> {
+	
+	public static final Logger LOGGER = Logger.getLogger(CustomerDaoMysql.class);
+
+	private String jdbcConnectionUrl;
+	private String username;
+	private String password;
+
+	public ItemDoaMysql(String username, String password) {
+		this.jdbcConnectionUrl = "jdbc:mysql://" + Utils.MYSQL_URL + "/ims_project";
+		this.username = username;
+		this.password = password;
+	}
+
+	public ItemDoaMysql(String jdbcConnectionUrl, String username, String password) {
+		this.jdbcConnectionUrl = jdbcConnectionUrl;
+		this.username = username;
+		this.password = password;
+	}
+
+	Item itemFromResultSet(ResultSet resultSet) throws SQLException {
+		Long id = resultSet.getLong("item_ID");
+		String item_name = resultSet.getString("item_name");
+		double price = resultSet.getDouble("item_price");
+		int quant_in_stock = resultSet.getInt("quant_in_stock");
+		return new Item(id, item_name, price, quant_in_stock);
+	}
+
+	/**
+	 * Reads all items from the database
+	 *
+	 * @return A list of items
+	 */
+	@Override
+	public List<Item> readAll() {
+		try (Connection connection = DriverManager.getConnection(jdbcConnectionUrl, username, password);
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery("select * from Items");) {
+			ArrayList<Item> items = new ArrayList<>();
+			while (resultSet.next()) {
+				items.add(itemFromResultSet(resultSet));
+			}
+			return items;
+		} catch (SQLException e) {
+			LOGGER.debug(e.getStackTrace());
+			LOGGER.error(e.getMessage());
+		}
+		return new ArrayList<>();
+	}
+
+	public Item readLatest() {
+		try (Connection connection = DriverManager.getConnection(jdbcConnectionUrl, username, password);
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery("SELECT * FROM Items ORDER BY item_ID DESC LIMIT 1");) {
+			resultSet.next();
+			return itemFromResultSet(resultSet);
+		} catch (Exception e) {
+			LOGGER.debug(e.getStackTrace());
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+	}
+
+	/**
+	 * Creates a items in the database
+	 *
+	 * @param customer - takes in a items object. id will be ignored
+	 */
+	@Override
+	public Item create(Item item) {
+		try (Connection connection = DriverManager.getConnection(jdbcConnectionUrl, username, password);
+				Statement statement = connection.createStatement();) {
+			statement.executeUpdate("insert into Items(item_name, quant_in_stock, price) values('" + item.getItem_name()
+					+ "','" + item.getQuant_in_stock() + "','" + item.getItem_price() + "')");
+			return readLatest();
+		} catch (Exception e) {
+			LOGGER.debug(e.getStackTrace());
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+	}
+
+	public Item readItem(Long id) {
+		try (Connection connection = DriverManager.getConnection(jdbcConnectionUrl, username, password);
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery("SELECT * FROM Items where item_ID = " + id);) {
+			resultSet.next();
+			return itemFromResultSet(resultSet);
+		} catch (Exception e) {
+			LOGGER.debug(e.getStackTrace());
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+	}
+
+	/**
+	 * Updates a item in the database
+	 *
+	 * @param items - takes in a item object, the id field will be used to
+	 *                 update that item in the database
+	 * @return
+	 */
+	@Override
+	public Item update(Item item) {
+		try (Connection connection = DriverManager.getConnection(jdbcConnectionUrl, username, password);
+				Statement statement = connection.createStatement();) {
+			statement.executeUpdate("UPDATE Items SET item_name=" + item.getItem_name() + ", quant_in_stock=" + item.getQuant_in_stock() + 
+					", price=" + item.getItem_price() + " where item_ID=" + item.getItem_ID());
+			return readItem(item.getItem_ID());
+		} catch (Exception e) {
+			LOGGER.debug(e.getStackTrace());
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+	}
+
+	/**
+	 * Deletes a item in the database
+	 *
+	 * @param id - id of the customer
+	 */
+	@Override
+	public void delete(long id) {
+		try (Connection connection = DriverManager.getConnection(jdbcConnectionUrl, username, password);
+				Statement statement = connection.createStatement();) {
+			statement.executeUpdate("delete from Items where item_ID = " + id);
+		} catch (Exception e) {
+			LOGGER.debug(e.getStackTrace());
+			LOGGER.error(e.getMessage());
+		}
+	}
+
+}
---
+++ src/main/java/com/qa/ims/persistence/domain/Item.java
@@ -0,0 +1,58 @@
+package com.qa.ims.persistence.domain;
+
+public class Item {
+	
+	private long item_ID;
+	private String item_name;
+	private double item_price;
+	private int quant_in_stock;
+	
+	
+	public Item(String item_name, double item_price, int quant_in_stock) {
+		super();
+		this.item_name = item_name;
+		this.item_price = item_price;
+		this.quant_in_stock = quant_in_stock;
+	}
+
+	public Item(long item_ID, String item_name, double item_price, int quant_in_stock) {
+		super();
+		this.item_ID = item_ID;
+		this.item_name = item_name;
+		this.item_price = item_price;
+		this.quant_in_stock = quant_in_stock;
+	}
+
+	public String getItem_name() {
+		return item_name;
+	}
+
+	public void setItem_name(String item_name) {
+		this.item_name = item_name;
+	}
+
+	public double getItem_price() {
+		return item_price;
+	}
+
+	public void setItem_price(double item_price) {
+		this.item_price = item_price;
+	}
+
+	public int getQuant_in_stock() {
+		return quant_in_stock;
+	}
+
+	public void setQuant_in_stock(int quant_in_stock) {
+		this.quant_in_stock = quant_in_stock;
+	}
+
+	public long getItem_ID() {
+		return item_ID;
+	}
+	
+	public String toString() {
+		return "ID:" + this.getItem_ID() + ", Name:" + this.getItem_name() + ", Price:" + this.getItem_price() + ", Quantity in stock:" + this.getQuant_in_stock(); 
+	}
+
+}
